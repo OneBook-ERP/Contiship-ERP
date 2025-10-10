1943cbae5510e4a9fbfbe39ea3f2b8ba3385520d
LEO DANIEL A <leodaniel0013@gmail.com>
2025-10-10 16:24:40 +0530

    Feat:Remove the function of after submission of sales invoice creation

--- contiship_erp/contiship_erp/doctype/outward_entry/outward_entry.py
+++ contiship_erp/contiship_erp/doctype/outward_entry/outward_entry.py
@@ -11,8 +11,8 @@ class OutwardEntry(Document):
     def validate(self):
         self.calculate_available_space()
 
-    def on_update_after_submit(self):
-        frappe.enqueue("contiship_erp.contiship_erp.doctype.outward_entry.outward_entry.create_container_sales_invoice", queue='default', job_name=f"Create Sales Invoice for {self.name}", outward_entry=self.name)
+    # def on_update_after_submit(self):
+    #     frappe.enqueue("contiship_erp.contiship_erp.doctype.outward_entry.outward_entry.create_container_sales_invoice", queue='default', job_name=f"Create Sales Invoice for {self.name}", outward_entry=self.name)
          
 
     def on_submit(self):        
